01b34fd7fa0c7d2586fcc179b302d6c91ae3fada
Kutlay SARIMEHMET <kutlaysarimehmet@gmail.com>
2018-02-23 20:29:22 +0300

    Eyvallah tuna

--- lineage.mk
+++ lineage.mk
@@ -23,7 +23,7 @@ PRODUCT_RELEASE_NAME := GM6
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/google/gm6/device_Heart.mk)
+$(call inherit-product, device/google/gm6/device_gm6.mk)
 
 # Screen Config
 TARGET_SCREEN_HEIGHT := 1280
